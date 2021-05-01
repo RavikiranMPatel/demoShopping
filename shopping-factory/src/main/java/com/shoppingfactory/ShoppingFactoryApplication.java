3bda1dd56af56fc8162a5cc0bbf74ea8a61b64c8
Ravikiran <rkmpatel8@gmail.com>
2021-05-01 12:13:39 +0530

    First Commit

---
+++ shopping-factory/src/main/java/com/shoppingfactory/ShoppingFactoryApplication.java
@@ -0,0 +1,12 @@
+package com.shoppingfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingFactoryApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingFactoryApplication.class, args);
+	}
+}
---
+++ shopping-factory/src/test/java/com/demoShoppingApp/shoppingfactory/ShoppingFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoShoppingApp.shoppingfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
